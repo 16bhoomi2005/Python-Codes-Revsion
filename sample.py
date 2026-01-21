317b0ba01f2721013cd5f86b85e59cef5659899d
16bhoomi2005 <bhoomivaishya06@gmail.com>
2026-01-22 02:19:01 +0530

    first commit for sample.py

---
+++ sample.py
@@ -0,0 +1,8 @@
+# this is example code
+# To understand comments in Python
+# using /n# for single line comments 
+# and /n""" for multi-line comments """
+# shortcut: Ctrl + / for single line comments
+
+
+print("Hello, World!")
