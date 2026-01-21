7074e17e960bdfa78cbe7c681687ca144518220d
16bhoomi2005 <bhoomivaishya06@gmail.com>
2026-01-22 03:07:47 +0530

    Adding two types in  typecasting.py

--- typecasting.py
+++ typecasting.py
@@ -1,3 +1,4 @@
+#Explicit Type casting-Done by programmer
 a="3"
 b="27"
 print(a+b)# output=327
@@ -12,4 +13,10 @@ print(a+int(b))#Output=30
 #Here we have typecasted b and added typecasted value of b to a 
  
 print(type(b))
+
+#Implicit Typecasting-Done By Python Interpreter itself
+a=3.9
+b=33
+print(a+b) #output=36.9
+
 
