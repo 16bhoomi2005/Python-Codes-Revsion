0c6eea863a22a2e93e1319a01316e231b400c7f5
16bhoomi2005 <bhoomivaishya06@gmail.com>
2026-01-22 03:03:59 +0530

    Adding typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,15 @@
+a="3"
+b="27"
+print(a+b)# output=327
+
+a=3
+b=27
+print(a+b) #output=30
+
+a=3
+b="27"
+print(a+int(b))#Output=30
+#Here we have typecasted b and added typecasted value of b to a 
+ 
+print(type(b))
+
