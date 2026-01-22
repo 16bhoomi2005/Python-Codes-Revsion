d07a008ea5ebd5d4759773d7dec6c7156638612f
16bhoomi2005 <bhoomivaishya06@gmail.com>
2026-01-22 13:48:35 +0530

    First commit string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,23 @@
+a="heLLo World"
+print(len(a))
+print(a.capitalize())#Capitalizes first letter of sentence
+print(a.lower())
+print(a.casefold())#Converts to lowercase
+print(len(a.center(50)))#Adds space in front such that the length becomes 50
+print(len(a))
+print(a.center(50))
+print(a.count("heLLo"))#Number of occurrence
+print(a.encode())
+print(a.endswith("ld"))#Boolean value
+print(a.index("o"))#Gives index of first occurence of o
+print(a.isalnum())
+print(a.isascii())
+print(a.isdecimal())
+print(a.islower())
+print(a.isprintable())
+b='  '
+print(b.isspace())
+print(a.swapcase())
+print(a.split())#Creates a list separeted by space nPython is a fun language"
+print(str.split())
+print(str.splitlines())#Splits by newline
